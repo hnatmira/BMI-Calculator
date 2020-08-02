64236186203b02ccad7749a12f680ec3e7f958d6
Miroslav Hnát <hnatmira@gmail.com>
2020-08-02 13:04:52 +0200

    Initial Commit

---
+++ BMI Calculator/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  BMI Calculator
+//
+//  Created by Miroslav Hnát on 02/08/2020.
+//  Copyright © 2020 Miroslav Hnát. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
