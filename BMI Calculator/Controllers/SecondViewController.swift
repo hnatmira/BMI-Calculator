56484beb79d174e246c43a400f8f17efd4444348
DZCSE8O <miroslav.hnat@celverlance.com>
2020-09-26 17:21:12 +0200

    try create second view for result

---
+++ BMI Calculator/Controllers/SecondViewController.swift
@@ -0,0 +1,9 @@
+//
+//  SecondViewController.swift
+//  BMI Calculator
+//
+//  Created by Miroslav Hnát on 18/08/2020.
+//  Copyright © 2020 Miroslav Hnát. All rights reserved.
+//
+
+import Foundation
